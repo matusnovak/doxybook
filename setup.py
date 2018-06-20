7b7761ff521686b8eb8ac1d377eeb74ec4f62cfd
Matus Novak <matusnov@gmail.com>
2018-06-20 19:21:45 +0100

    Bump up version number

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='doxybook',
-    version='2.0.0',
+    version='2.0.1',
     description='Doxygen XML to GitBook markdown',
     long_description=long_description,
     url='https://github.com/matusnovak/doxybook',
