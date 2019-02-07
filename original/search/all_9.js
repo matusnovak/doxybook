027c4372aaa14717914d20fb656468658247635c
Matus Novak <matusnov@gmail.com>
2019-02-07 01:02:55 +0000

    Updating examples

--- original/search/all_9.js
+++ original/search/all_9.js
@@ -2,5 +2,6 @@ var searchData=
 [
   ['some_20organism_20example',['Some organism example',['../group__organism.html',1,'']]],
   ['some_5fglobal_5ffunction',['some_global_function',['../classexample_1_1_animal.html#aa7ca55f69abe84eade027036327d6f34',1,'example::Animal::some_global_function()'],['../group__animals.html#gad4969d516ff95884b14e25cce5c8b507',1,'some_global_function():&#160;animal.h']]],
+  ['some_5finline_5fmember_5ffunction',['some_inline_member_function',['../classexample_1_1_animal.html#a6394d7fca31729753ed96bbad13d305a',1,'example::Animal']]],
   ['specialbird',['SpecialBird',['../classexample_1_1_special_bird.html',1,'example']]]
 ];
--- original/search/functions_4.js
+++ original/search/functions_4.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['some_5fglobal_5ffunction',['some_global_function',['../group__animals.html#gad4969d516ff95884b14e25cce5c8b507',1,'animal.h']]]
+  ['some_5fglobal_5ffunction',['some_global_function',['../group__animals.html#gad4969d516ff95884b14e25cce5c8b507',1,'animal.h']]],
+  ['some_5finline_5fmember_5ffunction',['some_inline_member_function',['../classexample_1_1_animal.html#a6394d7fca31729753ed96bbad13d305a',1,'example::Animal']]]
 ];
