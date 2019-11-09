99e9ac892f430a687b94768c738484089a4c4548
Matus Novak <matusnov@gmail.com>
2019-11-09 14:12:45 +0000

    Updates

--- original/search/all_c.js
+++ original/search/all_c.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['parents',['Parents',['../classexample_1_1Animal.html#a3fc3e692a61c2e21080fef955df099d5',1,'example::Animal']]],
-  ['pi',['PI',['../group__animals.html#ga598a3330b3c21701223ee0ca14316eca',1,'config.h']]]
+  ['pi',['PI',['../group__animals.html#ga598a3330b3c21701223ee0ca14316eca',1,'config.h']]],
+  ['print_5fpretty',['PRINT_PRETTY',['../group__animals.html#ga0e55411baf0086e23b30885e7d179b49',1,'config.h']]]
 ];
